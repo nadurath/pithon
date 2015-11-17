2be0db2518a37266d1c1a4879ca164828f711266
DAWOOD NADURATH <dadurath@gmail.com>
2015-11-17 14:13:19 -0600

    Added speech-to-text and text-to-speech functionality.

--- voiceControl.py
+++ voiceControl.py
@@ -3,16 +3,20 @@ import speech_recognition as sr
 from datetime import datetime
 import pywapi
 import string
+import pyttsx
 
 input = 'sdf'
 
 r = sr.Recognizer()
-s = ""
-#with sr.Microphone() as source:
-#	audio = r.listen(source)
-#	s = r.recognize_google(audio)
-#print s
 
+engine = pyttsx.init()
+
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source,duration=1)
+    print("How can I help you?")
+    audio = r.listen(source)
+
+req = r.recognize_google(audio)
 def tempSuggest():  # this function will suggest the type of clothes based on the temperature
     global string
     my_location = pywapi.get_weather_from_weather_com('75080')
@@ -38,16 +42,19 @@ def tempSuggest():  # this function will suggest the type of clothes based on th
 my_location = pywapi.get_weather_from_weather_com('75080')
 tempFah = int(int(my_location['current_conditions']['temperature'])*1.8 + 32)
 
-req = raw_input("How can I help you?\n").lower()
 if "weather" in req or "forecast" in req or "temperature" in req:
-    print ("\nIt is " + my_location['current_conditions']['text'].lower() + " and " + str(tempFah) + " degrees fahrenheit.")
-    print tempSuggest()
+    engine.say("\nIt is " + my_location['current_conditions']['text'].lower() + " and " + str(tempFah) + " degrees fahrenheit.")
+    engine.say(tempSuggest())
+    engine.runAndWait()
     chanceRain = int(my_location['forecasts'][1]['day']['chance_precip'])
     if(chanceRain is 0):
-        print "Skies look clear today!"
+        engine.say("Skies look clear today!")
+        engine.runAndWait()
     elif(chanceRain > 0):
-        print "There's a " + chanceRain + "percent chance of rain today. Plan accordingly."
+        engine.say("There's a " + chanceRain + "percent chance of rain today. Plan accordingly.")
 elif "music" in req or "play" in req or "tunes" in req or "song" in req:
-    print "Coming right up!"
+    engine.say("Coming right up!")
+    engine.runAndWait()
 elif "define" in req or "mean" in req or "definition" in req:
-    print "Let me look that up for you."
+    engine.say("Let me look that up for you.")
+    engine.runAndWait()
